ed9f65bd4fc67f19390d0d8e2cfa01990f80f5b8
Manish Athreya <ayerhtahsinam@gmail.com>
2020-10-27 19:07:17 +0530

    added javascript

---
+++ javascript.js/javascript.js
@@ -0,0 +1,9 @@
+let markHeight = 1.88;
+let markMass = 95;
+let johnsHeight = 1.76;
+let johnsMass = 85;
+let markHigherBMI;
+let bmi = markMass/markHeight**2;
+let bmi2 = johnsMass/johnsHeight**2;
+console.log(bmi,bmi2);
+console.log(bmi>bmi2);
